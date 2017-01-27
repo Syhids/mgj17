91e9eb89168e42955873a4dd14caba74fbe25688
Manuel Jiménez <mjimenez@habitissimo.com>
2017-01-27 22:03:52 +0100

    Init

---
+++ desktop/src/io/syhids/mgj17/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.syhids.mgj17.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.syhids.mgj17.Trumpocalypse;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Trumpocalypse(), config);
+	}
+}
